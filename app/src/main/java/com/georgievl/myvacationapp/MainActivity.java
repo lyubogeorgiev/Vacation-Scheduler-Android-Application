e670a912ceac947d22c0b06adf43616d99ce3b83
Lyubonir Georgiev <lyubo12georgiev@gmail.com>
2024-06-28 14:26:41 -0600

    C.  Design the application to include the following information, including appropriate GUI (graphical user interface) elements (e.g., navigation, input, and information) for each layout:
    •  list of vacations

--- app/src/main/java/com/georgievl/myvacationapp/MainActivity.java
+++ app/src/main/java/com/georgievl/myvacationapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.georgievl.myvacationapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -16,6 +19,8 @@ import com.georgievl.myvacationapp.entities.Vacation;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnVacationsList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +30,16 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        btnVacationsList = findViewById(R.id.btn_vacations);
+
+        btnVacationsList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, VacationsListActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
