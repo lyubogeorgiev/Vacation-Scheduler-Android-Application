a3575149beb5c0242f17d02572dfbce8e2784d02
Lyubonir Georgiev <lyubo12georgiev@gmail.com>
2024-06-29 14:51:09 -0600

    c.  Include validation that the input dates are formatted correctly.
    d.  Include validation that the vacation end date is after the start date.

--- app/src/main/java/com/georgievl/myvacationapp/VacationDetailsActivity.java
+++ app/src/main/java/com/georgievl/myvacationapp/VacationDetailsActivity.java
@@ -1,9 +1,11 @@
 package com.georgievl.myvacationapp;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,9 +13,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 public class VacationDetailsActivity extends AppCompatActivity {
 
     Button btnListExcursions;
+    DatePickerDialog.OnDateSetListener startDate;
+    DatePickerDialog.OnDateSetListener endDate;
+    final Calendar startCalendar=Calendar.getInstance();
+    final Calendar endCalendar=Calendar.getInstance();
+    Button btnStartDate;
+    Button btnEndDate;
+    Date startingDate;
+    Date endingDate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +52,94 @@ public class VacationDetailsActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        btnStartDate = findViewById(R.id.btn_startDate);
+        btnEndDate = findViewById(R.id.btn_endDate);
+
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        String currentDate=sdf.format(new Date());
+
+//        btnStartDate.setText(currentDate);
+//        btnEndDate.setText(currentDate);
+
+        btnStartDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Date date;
+                String info=btnStartDate.getText().toString();
+                try{
+                    startCalendar.setTime(sdf.parse(info));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                DatePickerDialog dpd = new DatePickerDialog(VacationDetailsActivity.this, startDate,startCalendar.get(Calendar.YEAR),
+                        startCalendar.get(Calendar.MONTH), startCalendar.get(Calendar.DAY_OF_MONTH));
+
+                dpd.getDatePicker().setMinDate(System.currentTimeMillis());
+                dpd.show();
+            }
+        });
+
+        btnEndDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Date date;
+                String info=btnEndDate.getText().toString();
+                try{
+                    startCalendar.setTime(sdf.parse(info));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                DatePickerDialog dpd = new DatePickerDialog(VacationDetailsActivity.this, endDate,startCalendar.get(Calendar.YEAR),
+                        startCalendar.get(Calendar.MONTH), startCalendar.get(Calendar.DAY_OF_MONTH));
+
+                dpd.getDatePicker().setMinDate(startingDate.getTime() + 86400000);
+                dpd.show();
+            }
+        });
+
+        startDate = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+
+                startCalendar.set(Calendar.YEAR, year);
+                startCalendar.set(Calendar.MONTH, monthOfYear);
+                startCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+
+                String myFormat = "MM/dd/yy"; //In which you need put here
+                SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+                startingDate = startCalendar.getTime();
+
+                btnStartDate.setText(sdf.format(startCalendar.getTime()));
+            }
+
+        };
+
+        endDate = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+
+                startCalendar.set(Calendar.YEAR, year);
+                startCalendar.set(Calendar.MONTH, monthOfYear);
+                startCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+
+                String myFormat = "MM/dd/yy"; //In which you need put here
+                SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+                endingDate = startCalendar.getTime();
+                btnEndDate.setText(sdf.format(startCalendar.getTime()));
+            }
+
+        };
     }
 }
